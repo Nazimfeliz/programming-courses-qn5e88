0976f99cdf7cbde030e78bcac238603754fbccfe
Nazimfeliz <91882772+Nazimfeliz@users.noreply.github.com>
2022-12-12 11:29:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-qn5e88
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-qn5e88)
---
+++ index.ts
@@ -0,0 +1,20 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('bonjour tous le monde');
+console.log('bonjour tout le monde');
+console.log('bonjour lahcen');
+
+let number: number;
+let number2: number;
+let number3: number;
+number = 100;
+number2 = 50;
+
+number3 = number;
+number = number2;
+number2 = number3;
+
+console.log(number);
+console.log(number2);
